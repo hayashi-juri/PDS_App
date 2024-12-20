d01b56a888d5469ae3fa682ae4028aab67c8f91d
hayashi-juri <hayashi.uoa.cn@gmail.com>
2024-12-20 09:33:40 +0900

    Initial Commit

---
+++ PDS_App/PDS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDS_AppApp.swift
+//  PDS_App
+//
+//  Created by Juri Hayashi on 2024/12/20.
+//
+
+import SwiftUI
+
+@main
+struct PDS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PDS_AppTests/PDS_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PDS_AppTests.swift
+//  PDS_AppTests
+//
+//  Created by Juri Hayashi on 2024/12/20.
+//
+
+import Testing
+
+struct PDS_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
